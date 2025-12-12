ecf09ee8a269ae12f8dcb8c5e1b10b259a7c9ff1
Gonxis <goncholo@gmail.com>
2025-12-12 10:38:03 +0100

    5-dispatching

--- index.js
+++ index.js
@@ -37,6 +37,7 @@
  * 3. Never produce any side effects.
  */
 
+// Reducer function
 const todos = (state = [], action) => {
     if (action.type === "ADD_TODO") {
         return state.concat([action.todo])
@@ -45,29 +46,37 @@ const todos = (state = [], action) => {
     return state
 }
 
-const createStore = () => {
+const createStore = (reducer) => {
     /**
      * The store should have four parts
      * 1. The state
-     * 2. Get the state.
-     * 3. Listen to changes on the state.
-     * 4. Update the state.
+     * 2. Get the state. (getState)
+     * 3. Listen to changes on the state. (subscribe)
+     * 4. Update the state. (dispatch)
      */
-}
 
-let state
-let listeners = []
-
-const getState = () => state
-
-const subscribe = (listener) => {
-    listeners.push(listener)
-    return () => {
-        listeners = listeners.filter(l => l !== listener)
+    let state
+    let listeners = []
+    
+    const getState = () => state
+    
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter(l => l !== listener)
+        }
+    }
+    
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach(listener => listener())
+    }
+    
+    return {
+        getState,
+        subscribe,
+        dispatch,
     }
 }
 
-return {
-    getState,
-    subscribe,
-}
+const store = createStore(todos)
