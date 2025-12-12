1bf1df98d7a1ff7190d71b263925646ce447dce9
Gonxis <goncholo@gmail.com>
2025-12-12 09:57:10 +0100

    2-listening

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ const createStore = () => {
 }
 
 let state
+let listeners = []
 
 const getState = () => state
 
+const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+        listeners = listeners.filter(l => l !== listener)
+    }
+}
+
 return {
-    getState
+    getState,
+    subscribe,
 }
